befb503e1d12935717afb242b10834eba004f423
issambb <3issam901@gmail.com>
2024-09-02 00:48:52 +0200

    ADD VOLITAILE

--- tn/smartdev/java/PokemonFactory.java
+++ tn/smartdev/java/PokemonFactory.java
@@ -1,10 +1,12 @@
 package tn.smartdev.java;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import tn.smartdev.java.utils.*;
 
 public class PokemonFactory {
 
-    public Pokemon getPokemon(String pokemonType) {
+    public @Nullable Pokemon getPokemon(@NotNull String pokemonType) {
         switch (pokemonType) {
             case "Land":
                 return new LandPokemon();
--- tn/smartdev/java/Singleton.java
+++ tn/smartdev/java/Singleton.java
@@ -3,20 +3,20 @@ package tn.smartdev.java;
 public class Singleton{
 
   // private field that refers to the object
-  private static Singleton singleObject;
+  private static volatile Singleton singleObject;
                                               
   private Singleton() {
      // constructor of the SingletonExample class
   }
 
   public static Singleton getInstance() {
-     // write code that allows us to create only one object
-     // access the object as per our need
-     if(singleObject==null){
-        singleObject= new Singleton();
-     }
+     /*
+      write code that allows us to create only one object
+      access the object as per our need
+     */
+      if(singleObject==null) singleObject = new Singleton();
      return singleObject;
   }
-  
+
 
 }
